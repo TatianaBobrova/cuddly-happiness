18a5a0202da1de8b92a3c557923b5500ea11b340
TatianaBobrova <145847346+TatianaBobrova@users.noreply.github.com>
2023-09-29 11:32:34 +0300

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -6,35 +6,35 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Number x: ");
-        int x = scanner.nextInt();
-        System.out.print("Number y: ");
-        int y = scanner.nextInt();
-        System.out.print("Operation: ");
-        int Operation = scanner.nextInt();
-        if (Operation == 1) {
-            System.out.println("+");
-        } else if (Operation == 2) {
-            System.out.println("-");
-        } else if (Operation == 3) {
-            System.out.println("*");
-        } else if (Operation == 4) {
-            System.out.println("/");
-        }
-        int Result;
-        switch (Operation) {
-            case 1:
-                System.out.printf("Result: %d", x+y);
-                break;
-            case 2:
-                System.out.printf("Result: %d", x-y);
-                break;
-            case 3:
-                System.out.printf("Result: %d", x*y);
-                break;
-            case 4:
-                System.out.printf("Result: %d",x/y);
-                break;
-        }
+        Calculator colc = new Calculator();
+        String close = "no";
+        do {
+            System.out.print("Number x: ");
+            int x = scanner.nextInt();
+            System.out.print("Number y: ");
+            int y = scanner.nextInt();
+            System.out.print("Operation: ");
+            char Operation = scanner.next().charAt(0);
+            switch (Operation) {
+                case '+':
+                    System.out.printf("Result: %d \n", x + y);
+                    break;
+                case '-':
+                    System.out.printf("Result: %d \n", x - y);
+                    break;
+                case '*':
+                    System.out.printf("Result: %d \n", x * y);
+                    break;
+                case '/':
+                    System.out.printf("Result: %d \n", x / y);
+                    break;
+                default:
+                    System.out.println("Error \n");
+                    close = scanner.next();
+
+            }
+
+        }while (!close.equals("yas"));
+
     }
 }
